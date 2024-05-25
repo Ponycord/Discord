d1762b5c6059436d27482d7a9d34ce8a4f0f40a6
catbleed <156821393+catbleed@users.noreply.github.com>
2024-05-25 12:20:13 +0200

    Add files via upload

---
+++ Assets/rules_message.js
@@ -0,0 +1,60 @@
+{
+    username: '᲼',
+    avatar_url: 'https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png',
+    content: "",
+    embeds: [{
+            "color": 16777215,
+            "image": {
+                "url": "https://raw.githubusercontent.com/Ponycord/Discord/main/Assets/rules_banner.png"
+            },
+            "footer": {
+                "text": "Last update",
+            },
+            "timestamp": new Date().toISOString()
+        },
+        {
+            "description": "- **The rules may be updated without further notices.** It is your responsibility to stay informed about any changes.\n- **Rules apply to all content on the server**, unless specified otherwise. This includes nicknames and avatars.\n- **Use common sense.** Not all possible scenarios can be covered by rules, so try to behave in a manner that contributes to a positive environment.\n- In addition to our rules, you are expected to be following Discord's [Terms of Service](https://discord.com/terms) and [Community Guidelines](https://discord.com/guidelines).",
+            "color": 16777215
+        },
+        {
+            "color": 46335,
+            "image": {
+                "url": "https://raw.githubusercontent.com/Ponycord/Discord/main/Assets/rules_general_rules_banner.png"
+            }
+        },
+        {
+            "description": "1. **Respectful Communication**\n - **Discrimination** based on race, gender, sexuality, religion, disability, opinions or any other status **is strictly prohibited**.\n -  **Bullying, harassment, hate speech, and personal attacks will not be tolerated.**\n2. **Appropriate Content**\n - **Do not share extreme or graphic content.** This includes but is not limited to excessively violent, gory, or sexually explicit material.\n - **Do not share any private information**, including addresses, phone numbers, or any other sensitive data.\n - **Do not post malicious links or files.**\n3. **Spam**\n - **Avoid excessive sending of messages or emojis.**\n - **Post content in the relevant channels.** Off-topic discussions should be moved to the appropriate channel.\n - Do not advertise, or share any kind of referral links without prior permission from staff.\n4. **Harmful Behavior**\n - **Do not impersonate** other members, staff, or public figures.\n - **Do not use any kind of automated scripts or self-bots**.",
+            "color": 46335
+        },
+        {
+            "color": 61246,
+            "image": {
+                "url": "https://raw.githubusercontent.com/Ponycord/Discord/main/Assets/rules_text_channels_banner.png"
+            }
+        },
+        {
+            "description": "## These rules apply to channels which they are listed under.\n### **<#1243527274184183808>**\n> - **Keep all discussions, images, and links safe for work.** No explicit or mature content.\n> - **Avoid arguments or heated debates.**\n\n### **<#1243527293205352499>**\n> - **Topics must remain within legal boundaries**.\n> - **Debates and arguments must be conducted respectfully without personal attacks**.\n> - **The use of explicit language is permitted, but excessive or inappropriate use will be evaluated on a case-by-case basis**.\n\n### **<#1243651322041860096>**\n> - Contents of all attachments must also follow the server rules.",
+            "color": 61246
+        },
+        {
+            "color": 16727646,
+            "image": {
+                "url": "https://raw.githubusercontent.com/Ponycord/Discord/main/Assets/rules_voice_channels_banner.png"
+            }
+        },
+        {
+            "description": "1. **Ensure your microphone is not too loud or disruptive to others**.\n2. **Use each voice channel for it's purpose**.",
+            "color": 16727646
+        },
+        {
+            "color": 16777215,
+            "image": {
+                "url": "https://raw.githubusercontent.com/Ponycord/Discord/main/Assets/rules_punishments_banner.png"
+            }
+        },
+        {
+            "description": "### **Rule 1: Respectful Communication** \n> - **First Offense**: Warning & 1-day mute\n> - **Second Offense**: 1-week mute\n> - **Multiple Offenses**: Permanent ban\n \n### **Rule 2: Appropriate Content**\n> - **First Offense**: Warning & 6-hour mute\n> - **Second Offense**: 1-week mute\n> - **Multiple Offenses**: Permanent ban\n \n### **Rule 3: Spam**\n> - **First Offense**: Warning\n> - **Second Offense**: 1-hour mute\n> - **Multiple Offenses**: 1-day mute\n \n### **Rule 4: Harmful Behavior**\n> - **First Offense**: Warning\n> - **Second Offense**: Kick\n> - **Multiple Offenses**: Temporary ban (1 month)",
+            "color": 16777215
+        }
+    ]
+}
